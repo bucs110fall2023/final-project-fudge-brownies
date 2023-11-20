1b019b47a9306780b70e1188bf0ba48d3640a3e0
BPNMDNM <143661383+BPNMDNM@users.noreply.github.com>
2023-11-20 17:32:53 -0500

    working on controller

--- src/sample_controller.py
+++ src/sample_controller.py
@@ -1,28 +1,48 @@
 import pygame
 from button import Button
 
-start_button = Button(text="Start")
 
 
 class Controller:
   
   def __init__(self):
     #setup pygame data
-    
+    self.start_button = Button(text='Start')
+    self.quit_button = Button(text = 'Quit')
+    self.
+    #menu buttons go there
+    self.state = 'Menu'
+    self.state = 'Gameloop'
     
   def mainloop(self):
     #select state loop
+    if self.state == 'Menu':
+       self.menuloop()
+    
+    elif self.state == 'Gameloop':
+       self.gameloop()
+
     
   
   ### below are some sample loop states ###
 
   def menuloop(self):
     
-      #event loop
+    
+    while self.state == 'Menu':
+   
+      for events in pygame.event.get():
+        if events.type == pygame.QUIT:
+            pygame.quit()
 
-      #update data
+        if events.type == pygame.MOUSEBUTTONDOWN:
 
-      #redraw
+          if self.start_button.collidepoint(events.pos):
+                self.state == "Gameloop"
+
+          elif pbutton.collidepoint(events.pos):
+                darts()
+                #function purple
       
   def gameloop(self):
       #event loop
@@ -31,9 +51,4 @@ class Controller:
 
       #redraw
     
-  def gameoverloop(self):
-      #event loop
-
-      #update data
-
-      #redraw
+  
