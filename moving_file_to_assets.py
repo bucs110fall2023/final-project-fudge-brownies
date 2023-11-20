0326a419d581be2eca8ed8783eb045d163efd74f
BPNMDNM <143661383+BPNMDNM@users.noreply.github.com>
2023-11-20 14:59:08 -0500

    working on it

--- moving_file_to_assets.py
+++ moving_file_to_assets.py
@@ -1,10 +1,14 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
-from PIL import Image
+from PIL import Image, ImageFilter
 
 
 source = askopenfilename()
-print(source)
+#print(source)
 im = Image.open(source)
-im.show()
 
+gray_im = im.convert("L")
+#blur_im = gray_im.filter(ImageFilter.BLUR)
+smooth_im = im.filter(ImageFilter.SMOOTH)
+filter_im = smooth_im.filter(ImageFilter.FIND_EDGES)
+filter_im.show()
