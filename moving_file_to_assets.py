877cfde425d3bc5671276dce9f8ac8a48d75f09e
BPNMDNM <143661383+BPNMDNM@users.noreply.github.com>
2023-11-20 16:17:15 -0500

    made image processing class

--- moving_file_to_assets.py
+++ moving_file_to_assets.py
@@ -2,13 +2,15 @@ import tkinter as tk
 from tkinter.filedialog import askopenfilename
 from PIL import Image, ImageFilter
 
-
-source = askopenfilename()
+class Img_Pro:
+    def img_process(self):
+    
+        source = askopenfilename()
 #print(source)
-im = Image.open(source)
+        im = Image.open(source)
 
-gray_im = im.convert("L")
-#blur_im = gray_im.filter(ImageFilter.BLUR)
-smooth_im = im.filter(ImageFilter.SMOOTH)
-filter_im = smooth_im.filter(ImageFilter.FIND_EDGES)
-filter_im.show()
+        gray_im = im.convert("L")
+        #blur_im = gray_im.filter(ImageFilter.BLUR)
+        smooth_im = im.filter(ImageFilter.SMOOTH)
+        filter_im = smooth_im.filter(ImageFilter.FIND_EDGES)
+        filter_im.show()
